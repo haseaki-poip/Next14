5ecf884853f07130cab2bcba1ef1a3c9d1d8fc5b
Hasegawa-Akito <h.akito1999@gmail.com>
2024-03-16 12:53:00 +0900

    add: codegenによる型の自動生成

--- src/app/graphql/page.tsx
+++ src/app/graphql/page.tsx
@@ -5,6 +5,7 @@ import {
   gql,
 } from "@apollo/client";
 import { cache } from "react";
+import { DogsQuery, DogsDocument } from "@/generated/codegen";
 
 // Thirdパーティなどを使用した通信部分の関数をcacheで囲み、revalidateの時間をせっていするとISRとなる
 // revalidateを0にするとSSRの動作となる
@@ -16,23 +17,14 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const DOGS = gql`
-  query Dogs {
-    dogs {
-      description
-      id
-      name
-      thumbnail {
-        url
-      }
-    }
-  }
-`;
 const getDogDatas = cache(async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   console.log("graph");
-  const { data } = await client.query({
-    query: DOGS,
+  // 型にcodegenで自動で生成した型を指定
+  const { data } = await client.query<DogsQuery>({
+    // codegenによって生成したものを使用
+    // そのままっ記述したクエリでも可能
+    query: DogsDocument,
     fetchPolicy: "no-cache",
     // Graphqlでこれを指定しずにサーバーでデータを取得する処理を行うと、サーバでキャッシュが残ったままとなる
     // client側でGraphqlを叩く時はリロードすれば大丈夫そう？
